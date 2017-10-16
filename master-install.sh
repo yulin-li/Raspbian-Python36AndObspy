9b33ddee91dce11924a478ca8dfa4b2710175a50
Yulin Li <liyulin@pku.edu.cn>
2017-10-16 16:40:46 +0800

    change to pwd after python installation

--- master-install.sh
+++ master-install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+DIR=`pwd`
+
 # Install python 3.6
 . config/python-3.6/py3.6-apt-requirements.sh
 . config/python-3.6/py3.6-install.sh
+
+cd ${DIR}
 
 # Install obspy from source
 . config/obspy/obspy-apt-requirements.sh
