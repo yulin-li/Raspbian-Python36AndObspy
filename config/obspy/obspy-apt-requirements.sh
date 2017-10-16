719abda9c00c0aad44d970c699b714930b33eed2
Yulin Li <liyulin@pku.edu.cn>
2017-10-16 15:00:52 +0800

    no need to update twice

--- config/obspy/obspy-apt-requirements.sh
+++ config/obspy/obspy-apt-requirements.sh
@@ -6,8 +6,3 @@ sudo apt-get -y upgrade
 # Install Obspy dependencies
 # Install the required build-tools (some might already be installed on your system).
 sudo apt-get -y install liblapack-dev libblas-dev libxml2 libxml2-dev libxmlsec1-dev
-
-
-# Update the system
-sudo apt-get -y update
-sudo apt-get -y upgrade
